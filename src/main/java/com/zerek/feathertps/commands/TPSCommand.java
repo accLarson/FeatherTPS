e693f96d686869fa582e49b47e8f27b602290a63
Derek <acc.larson@gmail.com>
2022-07-22 12:14:32 -0400

    Fixed /tps command

--- src/main/java/com/zerek/feathertps/commands/TPSCommand.java
+++ src/main/java/com/zerek/feathertps/commands/TPSCommand.java
@@ -3,6 +3,7 @@ package com.zerek.feathertps.commands;
 import com.zerek.feathertps.FeatherTPS;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,17 +16,39 @@ import java.util.Arrays;
 public class TPSCommand implements CommandExecutor {
 
     private final FeatherTPS plugin;
-    private final String tpsMessage;
+    private final String tpsMessage,tpsAllMessage;
 
     public TPSCommand(FeatherTPS plugin) {
         this.plugin = plugin;
         this.tpsMessage = plugin.getConfig().getString("tps-message");
+        this.tpsAllMessage = plugin.getConfig().getString("tps-all-message");
+    }
+
+    private String getTPS(int index){
+        double tps = plugin.getServer().getTPS()[index];
+        if (tps > 20.00) return "20.00";
+        else return String.format("%.2f", tps);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player || sender instanceof ConsoleCommandSender){
-            sender.sendMessage(MiniMessage.miniMessage().deserialize(tpsMessage, Placeholder.unparsed("tps", Arrays.toString(plugin.getServer().getTPS()))));
+            String TPSString;
+            switch (args.length) {
+                case 0:
+                    TPSString = getTPS(0);
+                    sender.sendMessage(MiniMessage.miniMessage().deserialize(tpsMessage, Placeholder.unparsed("tps", TPSString)));
+                    break;
+                case 1:
+                    if (args[0].equalsIgnoreCase("all")) {
+                        TPSString = getTPS(0) + " - " + getTPS(1) + " - " + getTPS(2);
+                        sender.sendMessage(MiniMessage.miniMessage().deserialize(tpsAllMessage, Placeholder.unparsed("tps", TPSString)));
+                    } else sender.sendMessage(ChatColor.of("#E4453A") + "Invalid Command");
+                    break;
+                default:
+                    sender.sendMessage(ChatColor.of("#E4453A") + "Invalid Command");
+                    break;
+            }
         }
         return true;
     }
--- src/main/java/com/zerek/feathertps/commands/TPSCommandTabCompleter.java
+++ src/main/java/com/zerek/feathertps/commands/TPSCommandTabCompleter.java
@@ -13,6 +13,19 @@ public class TPSCommandTabCompleter implements TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         List<String> options = new ArrayList<>();
+        options.add("all");
+//        switch (args.length) {
+//            case 0:
+//                return options;
+//            case 1:
+//                List<String> match = new ArrayList<String>();
+//                for (String topic : options)
+//                    if (topic.toLowerCase().startsWith(args[0].toLowerCase())) match.add(topic);
+//                return match;
+//            default:
+//                options.clear();
+//                return options;
+//        }
         return options;
     }
 }
